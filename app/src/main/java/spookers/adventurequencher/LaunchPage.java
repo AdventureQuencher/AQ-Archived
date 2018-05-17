c8e954b52c25faf987fee7c9cf04307ca20e1948
AreeMartyn <fvg6708@autuni.ac.nz>
2018-05-17 17:28:12 +1200

    Initial commit

---
+++ app/src/main/java/spookers/adventurequencher/LaunchPage.java
@@ -0,0 +1,13 @@
+package spookers.adventurequencher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_page);
+    }
+}
